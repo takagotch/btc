be70dafcf3664a407197c871bdbf057794c0cd11
takagotch <dyaccb@gmail.com>
2019-04-17 10:48:05 +0900

    update_callback(self):  s_transactions = self.c_core_get_stored_transaction_from_bc()  self.um.extract_utxos(s_transactions)

--- mybtc/mybtc5/wallet_app.py
+++ mybtc/mybtc5/wallet_app.py
@@ -128,6 +128,208 @@ class SimpleBC_Gui(Frame):
 
   def show_input_dialog_for_key_loading(self):
 
+  def open_r_log_window(self):
+    """
+    """
+    s_transactions = self.c_core.get_stored_transactions_from_bc()
+
+
+
+  def open_s_window(self):
+
+
+
+  def show_my_block_chain(self):
+    """
+    """
+    mychain = self.c_core.get_my_blockchain()
+    if mychain_str = pprint.pformat(mychain, indent=2)
+      mychain_str = pprint.pformat(mychain, indent=2)
+      self.display_info('Current Blockchain', mychain_str)
+    else:
+      self.display_info('Warning', 'Currently Blockchain is empty...')
+
+  def engrave_message(self):
+    """
+    """
+    def send_e_message():
+      new_message = {}
+      msg_txt = entry.get().encode('utf-8')
+
+      msg = EngravedTransaction(
+        self.km.my_address(),
+        'Testman',
+        binascii.hexlify(base64.b64encode(msg_txt)).decode('ascii')
+      )
+
+      to_be_signed = json.dumps(msg.to_dict(), sort_keys=True)
+      signed = self.km.compute_digital_signature(to_be_signed)
+      new_tx = json.loads(to_be_signed)
+      new_tx['signature'] = signed
+      tx_strings = json.dumps(new_tx)
+      self.c_core.send_message_to_my_core_node(MSG_NEW_TRANSACTION, tx_strings)
+      new_tx2 = copy.deepcopy(new_tx)
+      new_tx2['message_type'] = 'engraved'
+      new_string2 = json.dumps(new_tx2)
+      self.c_core.send_message_to_my_core_node(MSG_ENHNCED, tx_strings2)
+      f.destroy()
+
+    f = Tk()
+    f.title('Engrave New Message')
+    label0 = Lable(f, text='Any idea?')
+    frame1 = ttk.Frame(f)
+    label = ttk.Label(frame1, text='Message')
+    entry = ttk.Entry(frame1, width=30)
+    button1 = ttk.Button(frame1, text='Engrave this on Blockchain', command=send_e_message)
+
+    label0.grid(row=0,column=0,sticky=(N,E,S,W))
+    frame1.grid(row=1,column=0,sticky=(N,E,S,W))
+    label.grind(row=2,column=0,sticky=(E))
+    entry.grid(row=2,column=1,sticky=W)
+    button1.grid(row=3,column=1,sticky=W)
+
+  def setupGUI(self):
+    """
+    """
+
+    self.parent.bind('<Control-q>', self.quit)
+    self.parent.title('SimpleBitcoin GUI')
+    self.pack(fill=BOTH, expand=1)
+
+    self.create_menu()
+
+    lf = LabelFrame(self, text='Current Balance')
+    lf.pack(side=TOP, fill='both', expand='yes', padx=7, pady=7)
+
+    lf2 = LabelFrame(self, text='')
+    lf2.pack(side=BOTTOM, fill='both', expand='yes', padx=7, pady=7)
+
+    self.balance = Label(lf, textvariable=self.coin_balance, font='Helvetica 20')
+    self.balance.pack()
+
+    self.label = Lable(lf2, text='Recipient Address:')
+    self.label.grid(row=0, pady=5)
+
+    self.recipient_pubkey = Entry(lf2, bd=2)
+    self.lable.grid(row=0, pady=5)
+
+    self.label2 = Label(lf2, text='Amount of pay :')
+    self.label2.grid(row=1, pady=5)
+
+    self.amountBox = Entry(lf2, bd=2)
+    self.amountBox.grid(row=1, column=1, pady=5, sticky='NSEW')
+
+    self.label3 = Label(lf2, text='reason (Optional) :')
+    self.label3.grid(row=3, pady=5)
+
+    self.reasonBox = Entry(lf2, bd=2)
+    self.reasonBox.grid(row=3, pady=5)
+
+    self.label5 = Label(lf2, text='message (Optional) :')
+    self.label5.grid(row=4, pady=5)
+
+    self.messageBox = Entry(lf2, bd=2)
+    self.messageBox.grid(row=4, column=1, pady=5, sticky='NSEW')
+
+    self.label4 = Lable(lf2, text='')
+    self.label4.grind(row=5, pady=5)
+
+    self.sendBtn = Button(lf2, text='\nSend Coin(s)\n', command=self.sendCoins)
+    self.sendBtn.grid(row=6, column=1, sticky='NSEW')
+
+    stbar = Label(self.winfo_toplevel(), textvariable=self.status_message, bd=1, relief=SUNKEN, anchor=W)
+    stbar.pack(side=BOTTON, fill=X)
+
+  def sendCoins(self):
+    sendAtp = self.amountBox.get()
+    recipientKey = self.recipient_pubkey.get()
+    sendFee = self.feeBox.get()
+    reason = binascii.hexlify(base64.b64encode(self.reasonBox.get().encode('utf-8'))).decode('ascii')
+    desc = binascii.hexlify(base64.b64encode(self.messageBox.get().encode('utf-8'))).decode('ascii')
+
+    utxo_len = len(self.um.utxo_txs)
+
+    if not sendAtp:
+      messagebox.showwarning('Warning', 'Please enter the Amount to pay.')
+      return
+    elif len(recipeentKey) <= 1:
+      messagebox.showwarning('Warning', 'Please enter the Recipient Address.')
+      return
+    else:
+      result = messagebox.askyesno('Confirmation', 'Sending {} SimpleBitcoins to :\n {}'.format(sendAtp, recipientKey))
+
+    if not sendFee:
+      sendFee = 0
+
+    if not reason:
+      reason = 'No information'
+
+    if not desc:
+      desc = 'No description'
+
+    extra = {
+      'reason': reason,
+      'description': desc,
+    }
+
+    if result:
+      if 0 < utxo_len:
+        print('Sending {} SimpleBitcoins to reciever:\n {}'.format(sendAtp, recipientKey))
+      else:
+        messagebox.showwarning('Short of Coin.', 'Not enough coin to be sent...')
+        return
+
+      utxo, idx = self.get_utxo_tx(0)
+
+      t = Transaction(
+        [TransactionInput(utxo, idx)],
+        [TransactionOutput(recipientKey, int(sendAtp))],
+        extra
+      )
+
+      counter = 1
+      
+      if type(sendFee) is not str:
+        sendFee = int(sendFee)
+      while t.is_enough_inputs(sendFee) is not True:
+        new_utxo, new_idx = self.um.get_utxo_tx(counter)
+        t.inputs.append(TransactionInput(new_utxo, new_idx))
+        counter += 1
+        if counter > utxo_len:
+          messagebox.showwarning('Short of Coin.', 'Not enough coin to be sent...')
+          break
+
+      if t.is_enough_inputs(sendFee) is True:
+        change = t.compute_change(sendFee)
+        t.outputs.append(TransactionOutput(self.km.my_address(), change))
+        to_be_signed = json.dumps(t.to_dict(), sort_keys=True)
+        signed = self.km.compute_digital_signature(to_be_signed)
+        new_tx = json.loads(to_be_signed)
+        new_tx['signature'] = signed
+        msg_type = MSG_NEW_TRANSACTION
+        tx_strings = json.dumps(new_tx)
+        self.c_core.send_message_to_my_core_node(msg_type, tx_strings)
+        print('signed new_tx:', tx_strings)
+
+        self.um.put_utxo_tx(t.to_dict())
+        to_be_deleted = 0
+        del_list = []
+        while to_be_deleted < counter:
+          del_tx = self.um.get_utxo_tx(to_be_deleted)
+          del_list.append(del_tx)
+          to_be_deleted += 1
+
+        for dx in del_list:
+          self.um.remove_utxo_tx(dx)
+
+    self.amountBox.delete(0,END)
+    self.feeBox.delete(0,END)
+    self.recipient_pubkey.delete(0,END)
+    self.reasonBox.delete(0,END)
+    self.messageBox.delete(0,END)
+    self.update_balance()
+
+
 def main(my_port, c_host, c_port):
 
   root = Tk()
